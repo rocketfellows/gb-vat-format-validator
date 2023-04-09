7cadefe9af2bbb0b5834236f6e60f6ee7b32b966
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-04-09 16:19:45 +0400

    [CVFV-23] setup test.

--- tests/unit/GBVatFormatValidatorTest.php
+++ tests/unit/GBVatFormatValidatorTest.php
@@ -1,22 +1,21 @@
 <?php
 
-// TODO: set namespace
-namespace unit;
+namespace rocketfellows\GBVatFormatValidator\tests\unit;
 
-class CountryVatFormatValidatorTest extends TestCase
+use PHPUnit\Framework\TestCase;
+
+class GBVatFormatValidatorTest extends TestCase
 {
     /**
-     * TODO: set type
-     * @var
+     * @var GBVatFormatValidator
      */
     private $validator;
 
-    /**
-     * TODO: setup validator for test
-     */
     protected function setUp(): void
     {
-        $this->validator = new CountryVatFormatValidator();
+        parent::setUp();
+
+        $this->validator = new GBVatFormatValidator();
     }
 
     /**
